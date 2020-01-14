8c9253f3d69bcea395bb09c73e9d6885e343ffb0
BuildTools <unconfigured@null.spigotmc.org>
2020-01-14 15:38:59 -0500

    minor optimizations/organization

--- src/me/cjcrafter/schematicbrushes/Brush.java
+++ src/me/cjcrafter/schematicbrushes/Brush.java
@@ -1,11 +1,9 @@
 package me.cjcrafter.schematicbrushes;
 
 import com.sk89q.worldedit.EditSession;
-import com.sk89q.worldedit.EditSessionFactory;
 import com.sk89q.worldedit.WorldEdit;
 import com.sk89q.worldedit.WorldEditException;
 import com.sk89q.worldedit.bukkit.BukkitWorld;
-import com.sk89q.worldedit.command.argument.EditSessionHolder;
 import com.sk89q.worldedit.extent.clipboard.Clipboard;
 import com.sk89q.worldedit.function.operation.Operation;
 import com.sk89q.worldedit.function.operation.Operations;
@@ -21,32 +19,41 @@ import org.bukkit.inventory.ItemStack;
 import org.bukkit.inventory.meta.ItemMeta;
 
 import java.util.ArrayList;
+import java.util.HashMap;
 import java.util.List;
+import java.util.Map;
 import java.util.Objects;
 import java.util.Random;
+import java.util.stream.Collectors;
 
 public class Brush {
 
     private static final Random rand = new Random();
 
     private String name;
-    private List<Clipboard> schematics;
+    private Map<String, Clipboard> schematics;
 
     Brush(String name) {
         this.name = name.split("\\.")[1];
-        this.schematics = new ArrayList<>();
+        this.schematics = new HashMap<>();
 
-        API.getList("Brushes." + this.name + ".Schematics").forEach(str -> schematics.add(API.getSchematic(str)));
+        API.getList("Brushes." + this.name + ".Schematics").forEach(str -> schematics.put(str, API.getSchematic(str)));
     }
 
     /**
+     * Called whenever a player who has permission clicks at a block with this brush.
+     * Determines whether the brush is a scatter or normal brush, then proceeds to
+     * find locations to paste schematic(s)
      *
-     * @param player
-     * @param loc
+     * TODO Separate to click and scatter methods
+     *
+     * @param player Player to save Clipboard to (For //undo)
+     * @param loc Where the player clicked
      */
-    public void paste(Player player, Location loc) {
+    void paste(Player player, Location loc) {
         if (!API.getBool("Brushes." + name + ".Scatter.Enabled")) {
-            paste(schematics.get(rand.nextInt(schematics.size())), player, loc);
+
+            paste(getRandomSchematic(), player, loc);
             return;
         }
 
@@ -80,8 +87,8 @@ public class Brush {
 
             locations.add(currentLocation);
         }
-
-        locations.forEach(location -> paste(schematics.get(rand.nextInt(schematics.size())), player, location));
+        
+        locations.forEach(location -> paste(getRandomSchematic(), player, location));
     }
 
     /**
@@ -89,25 +96,27 @@ public class Brush {
      * @param loc The location for the x,z values, and to set the y
      */
     private void getGround(Location loc) {
-        API.getInstance().log(LogLevel.DEBUG, "&eAttempting to find ground at " + loc);
+        API.log(LogLevel.DEBUG, "Attempting to find ground at " + loc);
         int bound = API.getInt("Brushes." + name + ".Scatter.Ground.Bound");
         int lower = Math.max(loc.getBlockY() - bound, 0);
         int upper = Math.min(loc.getBlockY() + bound, API.getInt("Build_Height"));
-
+        API.log(LogLevel.DEBUG, "Checking from y" + upper + " to y" + lower);
+        
         for (int y = upper; y > lower; y--) {
             loc.setY(y);
             Block block = loc.getBlock();
             if (API.getList("Brushes." + name + ".Scatter.Ground.Ignore_Blocks").contains(block.getType().name())) {
-                API.getInstance().log(LogLevel.DEBUG, "y" + y + " is " + block.getType().name() + "...Blocked..." + upper + ">" + lower);
+                API.log(LogLevel.DEBUG, "y" + y + " is " + block.getType().name() + "...Blocked..." + upper + ">" + lower);
                 continue;
             }
 
-            API.getInstance().log(LogLevel.DEBUG, "&aBreaking at y= " + y + "(" + block.getType().name() + ")", null);
+            API.log(LogLevel.DEBUG, "&aBreaking at y= " + y + "(" + block.getType().name() + ")", null);
             break;
         }
     }
 
     private void paste(Clipboard schematic, Player player, Location loc) {
+        API.log(LogLevel.DEBUG, "Pasting at " + loc);
         try (EditSession editSession = WorldEdit.getInstance().getEditSessionFactory().getEditSession(new BukkitWorld(loc.getWorld()), -1)) {
             ClipboardHolder holder = new ClipboardHolder(schematic);
             holder.setTransform(getRotation());
@@ -120,11 +129,11 @@ public class Brush {
                     .ignoreAirBlocks(API.getBool("Brushes." + name + ".Ignore_Air"))
                     .build();
             Operations.complete(operation);
-            WorldEdit.getInstance().getSessionManager().findByName(player.getName()).remember(editSession);
+            Objects.requireNonNull(WorldEdit.getInstance().getSessionManager().findByName(player.getName())).remember(editSession);
         } catch (WorldEditException e) {
-            API.getInstance().log(LogLevel.ERROR, "Schematic \"" + schematic + "\" failed to load", e);
+            API.log(LogLevel.ERROR, "Schematic \"" + schematic + "\" failed to paste", e);
         } catch (NullPointerException e) {
-            API.getInstance().log(LogLevel.WARN, "Player \"" + player.getName() +
+            API.log(LogLevel.WARN, "Player \"" + player.getName() +
                     "\" has left the server while painting.", e);
         }
     }
@@ -138,11 +147,16 @@ public class Brush {
         double x = API.getDouble("Brushes." + name + ".Rotate_X");
         double y = API.getDouble("Brushes." + name + ".Rotate_Y");
         double z = API.getDouble("Brushes." + name + ".Rotate_Z");
-
         return new AffineTransform()
                 .rotateX(((int) x == -1) ? rand.nextInt(4) * 90 : x)
                 .rotateY(((int) y == -1) ? rand.nextInt(4) * 90 : y)
                 .rotateZ(((int) z == -1) ? rand.nextInt(4) * 90 : z);
+    }
+
+    @SuppressWarnings("SuspiciousMethodCalls")
+    private Clipboard getRandomSchematic() {
+        Object[] schematics = this.schematics.keySet().toArray();
+        return this.schematics.get(schematics[schematics.length - 1]);
     }
 
     /**
@@ -152,7 +166,9 @@ public class Brush {
     public ItemStack getItem() {
         ItemStack item = new ItemStack(Objects.requireNonNull(Material.getMaterial(API.getString("Brushes." + name + ".Material"))));
         ItemMeta meta = item.getItemMeta();
+        assert meta != null;
         meta.setDisplayName(API.color("&aSchematicBrushes~" + name));
+        meta.setLore(schematics.keySet().stream().map(str -> API.color("&2&o" + str)).collect(Collectors.toList()));
         item.setItemMeta(meta);
         return item;
     }
